727132a446e5127ba4bc10b53975d8c21b7d8008
GabrielDeml <gabrielddeml@gmail.com>
2020-12-20 14:59:57 -0500

    Adder seems to work

--- adder_tb.v
+++ adder_tb.v
@@ -1,26 +1,24 @@
 // Two-input and gate test bench
 `timescale 1s/100ms
-`include "halfAdder.v"
+`include "fullAdder.v"
 
 module adder_tb();
-reg in1; 
-reg in2;
-reg carryIn;
-wire sum;
-wire carryOut;
+reg in1_1, in1_2, in1_3, in1_4, in1_5, in1_6, in1_7, in1_8;
+reg in2_1, in2_2, in2_3, in2_4, in2_5, in2_6, in2_7, in2_8;
+wire Sum1, Sum2, Sum3, Sum4, Sum5, Sum6, Sum7, Sum8;
 
-halfAdder halfAdder(in1, in2, carryIn, sum, carryOut);
+fullAdder fullAdder(in1_1, in1_2, in1_3, in1_4, in1_5, in1_6, in1_7, in1_8, 
+in2_1, in2_2, in2_3, in2_4, in2_5, in2_6, in2_7, in2_8,
+Sum1, Sum2, Sum3, Sum4, Sum5, Sum6, Sum7, Sum8,
+carry1, carry2, carry3, carry4, carry5, carry6, carry7, carry8
+);
 initial
 begin
-    $monitor("in1=%b", "b=%b", "y=%b", "x=%b",in1, in2, carryIn);
+    // $monitor("in1=%b", "b=%b", "y=%b", "x=%b",in1, in2, carryIn);
     $dumpfile("adder.vcd");
     $dumpvars(0, adder_tb);
-    in1 = 0; in2 = 0; carryIn = 0; #10;
-    in1 = 0; in2 = 0; carryIn = 1; #10;
-    in1 = 0; in2 = 1; carryIn = 0; #10;
-    in1 = 1; in2 = 0; carryIn = 1; #10;
-    in1 = 1; in2 = 0; carryIn = 0; #10;
-    in1 = 1; in2 = 1; carryIn = 1; #10;
+    in1_1 = 1; in1_2 = 0; in1_3 = 0; in1_4 = 0; in1_5 = 0; in1_6 =0; in1_7 = 0; in1_8 = 0; in2_1 = 1; in2_2 = 0; in2_3 = 0; in2_4 = 0; in2_5 = 0; in2_6 = 0; in2_7 = 0; in2_8 = 0; #1;
+    in1_1 = 0; in1_2 = 0; in1_3 = 1; in1_4 = 0; in1_5 = 0; in1_6 =0; in1_7 = 1; in1_8 = 0; in2_1 = 1; in2_2 = 0; in2_3 = 1; in2_4 = 0; in2_5 = 0; in2_6 = 0; in2_7 = 0; in2_8 = 0; #1;
     $finish;
 end
 endmodule
--- fullAdder.v
+++ fullAdder.v
@@ -1,13 +1,18 @@
+`include "halfAdder.v"
 module fullAdder(
-    input in1_1, in1_2, in1_3, in1_4, in1_5, in1_6, in1_7, in1_8;
-    input in2_1, in2_2, in2_3, in2_4, in2_5, in2_6, in2_7, in2_8;
-    output num1, num2, num3, num4, num5, num6, num7, num8;
+    input in1_1, in1_2, in1_3, in1_4, in1_5, in1_6, in1_7, in1_8,
+    input in2_1, in2_2, in2_3, in2_4, in2_5, in2_6, in2_7, in2_8,
+    output Sum1, Sum2, Sum3, Sum4, Sum5, Sum6, Sum7, Sum8,
+    output carry1, carry2, carry3, carry4, carry5, carry6, carry7, carry8
 );
+// wire carry1, carry2, carry3, carry4, carry5, carry6, carry7, carry8;
 
-assign firstAdd = ;
-assign carry1 = in1 & in2;
-assign num2 = in3 ^ carry1;
-assign carry2 = in3 & carry1;
-
-// assign num3 = in4 ^ carry2;
+halfAdder halfAdder1(in1_1, in2_1, 1'b0, Sum1, carry1);
+halfAdder halfAdder2(in1_2, in2_2, carry1, Sum2, carry2);
+halfAdder halfAdder3(in1_3, in2_3, carry2, Sum3, carry3);
+halfAdder halfAdder4(in1_4, in2_4, carry3, Sum4, carry4);
+halfAdder halfAdder5(in1_5, in2_5, carry4, Sum5, carry5);
+halfAdder halfAdder6(in1_6, in2_6, carry5, Sum6, carry6);
+halfAdder halfAdder7(in1_7, in2_7, carry6, Sum7, carry7);
+halfAdder halfAdder8(in1_8, in2_8, carry7, Sum8, carry8);
 endmodule
