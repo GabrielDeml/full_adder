a898f3c77cd51ad55793bd6fcfea8aad14a47670
GabrielDeml <gabrielddeml@gmail.com>
2020-12-20 10:34:24 -0500

    Added half adder

--- halfAdder.v
+++ halfAdder.v
@@ -1,3 +1,11 @@
 module halfAdder(
-    input 
-)
+    input in1, in2, carryIn;
+    output sum, carryOut;
+);
+
+assign num1 = in1 ^ in2;
+assign sum = num1 ^ carryIn;
+assign and1 = num1 & carryIn;
+assign and2 = in1 & in2;
+assign carryOut = and1 | and2;
+endmodule
